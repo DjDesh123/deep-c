d10401bd9c9b7efb2e536ff157145b1a4b1eba70
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-14 17:04:00 +0100

    tested running

--- 001-basic-syntax-review/main.c
+++ 001-basic-syntax-review/main.c
@@ -1,3 +1,6 @@
-//
-// Created by Deshawn Johal on 14/04/2025.
-//
+#include <stdio.h>
+int main() {
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+    return 0;
+}
